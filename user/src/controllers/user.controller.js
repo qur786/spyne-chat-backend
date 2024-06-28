6291a8ce37bc820771af6cd32462b3fddc70cd91
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-28 17:29:43 +0530

    feat: added user endpoints

--- user/src/controllers/user.controller.js
+++ user/src/controllers/user.controller.js
@@ -1,10 +1,86 @@
-import { UserModel } from "../models/user.model";
+import { UserModel } from "../models/user.model.js";
 
 export async function createUser(req, res) {
   try {
     const user = new UserModel(req.body);
     await user.save();
     res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export async function updateUser(req, res) {
+  try {
+    const user = await UserModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export async function deleteUser (req, res) {
+  try {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export async function getUser (req, res) {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export async function listUsers (_req, res) {
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export async function searchUsers(req, res) {
+  try {
+    const { q } = req.query;
+    console.log(q);
+    const regex = new RegExp(q, 'i');
+    const users = await UserModel.find({
+        name: { $regex: regex }
+    });
+    res.send(users);
+  } catch (error) {
+    console.log(error)
+    res.status(400).send(error);
+  }
+};
+
+export async function followUser(req, res) {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    if (!user.followers.includes(req.user._id)) {
+      user.followers.push(req.user._id);
+      await user.save();
+    }
+    res.send(user);
   } catch (error) {
     res.status(400).send(error);
   }
--- user/src/index.js
+++ user/src/index.js
@@ -1,13 +1,32 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import { isAuthenticated } from "./middlewares/auth.middleware.js";
+import mongoose from "mongoose";
+import { createUser, updateUser, deleteUser, getUser, listUsers, searchUsers, followUser } from "./controllers/user.controller.js";
+
+import 'dotenv/config';
 
 const app = express();
 
-app.get("/", (_req, res) => {
-    res.json({ message: "Welcome Home!" });
-})
+app.use(json());
+app.use(urlencoded({
+  extended: false
+}));
 
-// Start the server
-const port = process.env.PORT ?? 3001;
-app.listen(port, () => {
-  console.log(`user micro-service is listening on ${port}`);
-});
+app.post('/', createUser);
+app.put('/:id', isAuthenticated, updateUser);
+app.delete('/:id', isAuthenticated, deleteUser);
+app.get('/search', searchUsers); // This should come before '/:id'
+app.get('/:id', getUser);
+app.get('/', listUsers);
+app.post('/:id/follow', isAuthenticated, followUser);
+
+mongoose.connect(process.env.MONGO_URL ?? "").then(() => {
+  console.log("Connected to database")
+  
+  // Start the server
+  const port = process.env.PORT ?? 3001;
+  app.listen(port, () => {
+    console.log(`user micro-service is listening on ${port}`);
+  });
+})
+.catch(console.log)
